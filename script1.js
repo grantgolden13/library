49c29da24689827c76fd6a83c2108f64f434066d
Grant Golden <grantgolden13@gmail.com>
2022-01-25 11:26:55 -0800

    library works except for 'have you read it?' part

--- script1.js
+++ script1.js
@@ -1,9 +1,10 @@
 const modal = document.querySelector('.modal');
-const btn = document.querySelector('.btn');
+const addBookBtn = document.querySelector('.btn');
+const gridContainer = document.querySelector('main');
 
 // reveals hidden form
 
-btn.addEventListener('click', function() {
+addBookBtn.addEventListener('click', function() {
     modal.style.display = "block";
 });
 
@@ -15,9 +16,6 @@ window.addEventListener('click', function(e) {
     }
 });
 
-// so i need to figure out how to link an object constructor
-// to this callback function below
-
 const Book = function(title, author, pages, isRead) {
     this.title = title;
     this.author = author;
@@ -25,23 +23,56 @@ const Book = function(title, author, pages, isRead) {
     this.isRead = isRead;
 }
 
+Book.prototype.turnBookToElement = function() {
+    let newBookCell = document.createElement("div");
+    newBookCell.classList.add('cell');
+    let titleP = document.createElement("p");
+    let titleT = document.createTextNode(`Title: ${this.title}`);
+    titleP.appendChild(titleT);
+    newBookCell.appendChild(titleP);
+    let authorP = document.createElement("p");
+    let authorT = document.createTextNode(`Author: ${this.author}`);
+    authorP.appendChild(authorT);
+    newBookCell.appendChild(authorP);
+    let pagesP = document.createElement("p");
+    let pagesT = document.createTextNode(`# of Pages: ${this.pages}`);
+    pagesP.appendChild(pagesT);
+    newBookCell.appendChild(pagesP);
+    let readP = document.createElement("p");
+    let readT = document.createTextNode(`Read?: ${this.isRead}`);
+    readP.appendChild(readT);
+    newBookCell.appendChild(readP);
+    gridContainer.appendChild(newBookCell);
+}
+
 // on submit, new book object gets created and
 // added to the array myLibrary
 
-const submit = document.getElementById("submit");
+let myLibrary = [];
 
-submit.addEventListener('click', function(e) {
+const submitBtn = document.getElementById("submit");
+
+submitBtn.addEventListener('click', function(e) {
     e.preventDefault();
-    let title = document.getElementById("title").value;
-    console.log(title);
+    addBookToLibrary();
+    modal.style.display = "none";
 });
 
+function addBookToLibrary() {
+    let title = document.getElementById("title").value;
+    let author = document.getElementById("author").value;
+    let pages = document.getElementById("pages").value;
+    let isRead = (document.getElementById("isRead").value == "checked" ? 
+        "Have read it." : "Not read yet");
+    const book = Object.create(Book.prototype, { title: { value: title },
+        author: { value: author },
+        pages: { value: pages },
+        isRead: { value: isRead } });
+    myLibrary.push(book);
+    book.turnBookToElement();
+}
 
-
-
-
-// loop over myLibrary, create an element for each object
-// and append that element to <main>
-
+// function turn books into elems and append them
+// as visible DOM/HTML elements
 
 
